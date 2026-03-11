aeec2c3a2884851a3aabe528ea95f4510d925aa7
WallDy <134301089+duy08k4@users.noreply.github.com>
2026-03-11 22:08:48 +0700

    Merge def to staging: Fix paths in RoomAdminLayout (#7)
    * style(ui/home): generate ui and set up dark mode
    
    * style(ui/projects): generate ui and respnsive
    
    * style(ui/signin): generate ui and restructure directory
    
    * style(ui/signup): generate ui
    
    * style(ui/signup)(ui/signin): responsive
    
    * style(ui/projectsdetail): generate ui
    
    * style(ui/resetpassword): generate ui
    
    * style(ui/usermanagement): generate ui
    
    * style(ui/classmanagement): generate ui
    
    * style(ui/milestones): generate ui
    
    * style(ui/forms): generate ui
    
    * style(ui/Main): generate ui. This page shows classes
    
    * style(ui/Main): generate JoinClassForm and NewClassForm components on the Main page
    
    * style(ui/RAOverview): generate ui
    
    * style(ui/RAMilestones): generate ui
    
    * style(ui/SAScoreBoardManagement): generate ui
    
    * style(ui/SASubmission): generate ui
    
    * style(ui/RAMembsers): generate ui
    
    * style(ui/RAForms): generate ui
    
    * style(ui/RAScoreboards): generate ui
    
    * style(ui/RASubmissions): generate ui
    
    * fix(RAMembers): fix error deployment is 'isOpenPopup' is declared but its value is never read.
    
    * fix(RAMembers): fix error deployment is 'isOpenPopup' is declared but its value is never read.
    
    * chore: add vercel.json
    
    * style(ui/SDMember): generate ui
    
    * style(ui/SDMilestones): generate ui
    
    * style(ui/SDScoreboard): generate ui
    
    * style(ui/SDSubmission): generate ui
    
    * style(page/lecturer): generate all uis for lecture role
    
    * fix: adjusting paths in RoomAdminLayout

--- src/ui/layouts/RoomAdminLayout.tsx
+++ src/ui/layouts/RoomAdminLayout.tsx
@@ -21,14 +21,14 @@ const RoomAdminLayout: React.FC = () => {
             </svg>
             ,
             label: "Tổng quan",
-            path: `/main/class/${classId}`
+            path: `/main/admin/class/${classId}`
         },
         {
             icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5 dark:stroke-white">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
             </svg>,
             label: "Thành viên",
-            path: `/main/class/${classId}/members`
+            path: `/main/admin/class/${classId}/members`
         },
         {
             icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5 dark:stroke-white">
@@ -37,7 +37,7 @@ const RoomAdminLayout: React.FC = () => {
 
             ,
             label: "Tiến trình",
-            path: `/main/class/${classId}/milestones`
+            path: `/main/admin/class/${classId}/milestones`
         },
         {
             icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" className="size-5 dark:stroke-white">
@@ -45,7 +45,7 @@ const RoomAdminLayout: React.FC = () => {
             </svg>
             ,
             label: "Biểu mẫu",
-            path: `/main/class/${classId}/forms`
+            path: `/main/admin/class/${classId}/forms`
         },
         {
             icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5 dark:stroke-white">
@@ -53,7 +53,7 @@ const RoomAdminLayout: React.FC = () => {
             </svg>
             ,
             label: "Bảng điểm",
-            path: `/main/class/${classId}/scoreboards`
+            path: `/main/admin/class/${classId}/scoreboards`
         },
         {
             icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" className="size-5 dark:stroke-white">
@@ -61,7 +61,7 @@ const RoomAdminLayout: React.FC = () => {
             </svg>
             ,
             label: "Bài nộp",
-            path: `/main/class/${classId}/submission`
+            path: `/main/admin/class/${classId}/submission`
         }
     ])
 
